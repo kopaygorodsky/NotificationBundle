0e990d117928454fb0efe48a59738b6dbf41b3f0
kopaygorodsky <vlad.kopaygorodsky@gmail.com>
2017-10-26 16:26:43 +0300

    clean up controller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,17 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $this->get('kopaygorodsky_notification.websockets.auth_provider');
-        \Ratchet\Client\connect('ws://localhost:8080')->then(function($conn) {
-            $data = ['receiver' => 'conn_59f146516d8ad7.11422006', 'message' => 'info'];
-
-            $conn->send(json_encode($data));
-            $conn->close();
-        }, function ($e) {
-            echo "Could not connect: {$e->getMessage()}\n";
-        });
-
-        die();
     }
 
 }
