a262ea03f04a10a8fe23e58750a5bcde9a99cb94
kopaygorodsky <vlad.kopaygorodsky@gmail.com>
2017-11-14 20:18:41 +0200

    renamed config

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -25,7 +25,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('kopay_notification')->isRequired();
+        $rootNode    = $treeBuilder->root('kopay_notify')->isRequired();
 
         $rootNode
             ->children()
